c0ba6675bcb8dee67f54ad401efc2c60307305cf
P Jones <pwallacejones@googlemail.com>
2017-12-11 11:47:56 +0000

    Full app commit- static components'

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ' ./Playlist.css';
+import TrackList from '../TrackList/TrackList';
+
+
+
+class Playlist extends React.Component {
+		constructor (props) {
+		super (props);
+	}
+	render () {
+		return (
+			<div className="Playlist">
+  			<input defaultValue={'New Playlist'}/>
+  			<!-- Add a TrackList component -->
+  			<a className="Playlist-save">SAVE TO SPOTIFY</a>
+			</div>
+		);
+	}
+}
+
+export default Playlist;
