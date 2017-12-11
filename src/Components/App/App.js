0cede8d8dcfe396eca926e6134b162ed7e04f3f8
P Jones <pwallacejones@googlemail.com>
2017-12-11 13:10:01 +0000

    exercise 34 push, bug check

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,9 @@ import './App.css';
 class App extends Component {
     constructor (props) {
     super (props);
+    this.state = {
+      "searchResults": []
+    };
   }
   render() {
     return (
@@ -13,22 +16,11 @@ class App extends Component {
       <div className="App">
       <!-- Add a SearchBar component -->
       <div className="App-playlist">
-      <!-- Add a SearchResults component -->
+      <SearchResults searchResults = {this.state.searchResults} />
       <!-- Add a Playlist component -->
       </div>
       </div>
-    </div>)
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    </div>
     );
   }
 }
