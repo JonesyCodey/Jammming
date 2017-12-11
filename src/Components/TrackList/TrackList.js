bea6cf902f01fa14c267c420f4dab68501b2fd0e
P Jones <pwallacejones@googlemail.com>
2017-12-11 13:43:17 +0000

    Second commit

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,13 +4,18 @@ import ' ./TrackList.css';
 
 
 class Tracklist extends React.Component {
-	render (){
-		return (
-			<div className="TrackList">
-    		{this.props.tracks.map(<Track key={track.id} />)}
-			</div>
-				);
-	}
+ render() {
+    return (
+      <div className="TrackList">
+        {
+          this.props.tracks.map(track => {
+            return <Track track={track}
+                          key={track.id}/>
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default TrackList;
